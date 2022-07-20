60d12dd352eebacfc3486d56c5eeae038eb5ab2f
dangphat1412 <phatnguyen1412@gmail.com>
2022-07-20 17:06:58 +0700

    Add Env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    GOOGLE_MAP_API: "AIzaSyBPrWdOpVJ4LhAiLCLSfwK-0i3E1q27R7s",
-    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/dlwn4tpuz/image/upload/",
+    GOOGLE_MAP_API: process.env.GOOGLE_MAP_API,
+    CLOUDINARY_URL: process.env.CLOUDINARY_URL,
   },
 };
